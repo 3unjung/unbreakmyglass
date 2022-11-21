6eb0d872ebabc0ed298e2fc3128f67bc4c7b2d39
3unjung <margaux.aclo1941@gmail.com>
2022-11-21 15:49:51 +0100

    Initial commit

---
+++ lib/partials/cus_appbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TheAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TheAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+
+  // return notre navbar
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/banner/umg.png',),
+        ],
+      ),
+
+    );
+  }
+}
